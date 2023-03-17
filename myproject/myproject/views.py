9fc6ebe35dcc984d59162809a5a6dd26d827d925
Tejas <adhalraotejas77@gmail.com>
2023-03-17 06:23:50 +0530

    third commit get request-1

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -4,6 +4,7 @@ def index(request):
     return render(request,'index.html')
 
 def about(request):
-    data={   'name':"RutVikk",
-              'age':'21',    }
+    name=request.GET.get('name')
+    age=request.GET.get('age')
+    data={ 'name':name,'age':age }
     return render(request,'about.html',data)
